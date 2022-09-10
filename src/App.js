ddaddfb0a13a20a45d0098e9e7f68908a885ed25
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2022-09-11 01:20:49 +0530

    learned JSX

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/welcome';
+import Hello from './components/hello';
 
 function App() {
   return (
     <div className="App">
 
-      <Greet />
-      <Welcome />
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello />
       
     </div>
   );
